f3872fc842bf090a615fb8596783ec31f3f87a2e
Le Bon B' Bauma <baumabulondo@gmail.com>
2022-04-01 05:00:35 +0300

    Build: Wrapping the Swift code into a function that collects the text output

--- Swift WebScraper/main.swift
+++ Swift WebScraper/main.swift
@@ -8,11 +8,28 @@
 import Foundation
 import SwiftSoup
 
+func scrapeHousePlantSpecies(url: URL) throws -> String {
+    
+    let html = try String(contentsOf: url)
+    let document = try SwiftSoup.parse(html)
+
+    let descriptionSpan = try document.select("#Description").first()!
+
+    let h2 = descriptionSpan.parent()!
+    var element = h2
+
+    var result = ""
+    while true
+                {
+        guard let sibling = try element.nextElementSibling() else { break }
+        if sibling.tagName() != "p" {
+            break
+        }
+        result += try sibling.text()
+        element = sibling
+    }
+    return result
+}
+
 let url = URL(string: "https://en.wikipedia.org/wiki/Aglaonema")!
-let html = try String(contentsOf: url)
-let document = try SwiftSoup.parse(html)
-
-let description = try document.select("#Description").first()!
-
-print(description)
-
+print(try scrapeHousePlantSpecies(url: url))
