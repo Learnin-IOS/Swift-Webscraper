6dd47c9d2d5e4ad5d39596acd8d02dce31b68190
Le Bon B' Bauma <baumabulondo@gmail.com>
2022-04-01 05:54:54 +0300

    feat: Collecting the desired data from specific tags. In this case h3 and ul tags

--- Swift WebScraper/main.swift
+++ Swift WebScraper/main.swift
@@ -13,25 +13,31 @@ func scrapeHouseplant(url: URL) throws {
     let html = try String(contentsOf: url)
     let document = try SwiftSoup.parse(html)
     
-    let span = try document.select(
-        "#List_of_common_houseplants")
-            .first()!
-        
-        let h2 = span.parent()!
-        var element = h2
-        
-        
-        while true
-        {
-            guard let sibling = try element.nextElementSibling() else { break }
-            print(sibling.tagName())
-            if sibling.tagName() == "h2" {
-                break
+    let span = try document.select("#List_of_common_houseplants").first()!
+    let h2 = span.parent()!
+    var element = h2
+    
+    
+    while true
+    {
+        guard let sibling = try element.nextElementSibling() else { break }
+        print(sibling.tagName())
+        switch sibling.tagName(){
+        case "h2":
+            return
+        case "h3":
+            print(try sibling.children().first()!.text())
+        case "ul":
+            for child in sibling.children() {
+                print(" ", try child.text())
             }
-            element = sibling
+        default:
+            break
         }
-        }
-        
-        let url = URL(string: "https://en.wikipedia.org/wiki/Houseplant")!
-        print(try scrapeHouseplant(url: url))
+        element = sibling
+    }
+}
+
+let url = URL(string: "https://en.wikipedia.org/wiki/Houseplant")!
+print(try scrapeHouseplant(url: url))
 
